e07fed8222edac65ea134ced693be1245ec47571
Leah Greenstein <lrg79@cornell.edu>
2016-08-02 18:32:38 -0400

    Update prob3set1.c

--- prob3set1.c
+++ prob3set1.c
@@ -1,44 +1,29 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 int main(){
 	char encrypted[] = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
-	int a = 33;
-	char hexvals[sizeof(encrypted)-1];
-	char xored[sizeof(encrypted)-1];
-	//store so is hexadecimal values
-	for(int i = 0; i < sizeof(xored)-1; i++){
-		 sscanf(&encrypted[i], "%01hhx", &hexvals[i]);
+	char hexvals[(sizeof(encrypted)-1)/2];
+	char xored[(sizeof(encrypted)-1)/2];
+
+	for(int i = 0; i < sizeof(encrypted)-1; i+=2){
+		 sscanf(&encrypted[i], "%02hhx", &hexvals[i/2]);
 	}
 
-	printf("%x", hexvals);
-	
-	while(a<=126){
-		int boolean = 1;
-		
-		char c = a; //turning int into ascii
-
-		//xoring items in encrypted with char c
-		// and storing into xored
-		for(int i = 0; i < sizeof(encrypted)-1; i++){
-			char c1 = encrypted[i] ^ c;
-			sscanf(&xored[i], "%c", &c1);
-		}
-
-		//checking if all english
-		for(int i = 0; i < sizeof(xored)-1; i++){
-			int a = xored[i];
-			if(!((a > 32) && (a < 91))){
-				boolean = 0;
-			}
-		}
-
-		if(boolean == 1){
-			printf(xored);
-			a = 126;
-		}else{
-			a++;
-		}
-	}
-	return 0;
+   printf("%s", hexvals);
+   int highest = 0;
+   for(int i = 0; i <= 127; i++){
+      int score = 0;
+      for(int j = 0; j < sizeof(hexvals); j++){
+         char c1 = i ^ hexvals[j];
+         xored[j] = c1;
+         if(((c1 >= 65 && c1 <= 90) || (c1 == 32 )|| (c1 >= 97 && c1 <= 122))){
+            score++;
+         }
+      }
+      if(score >= highest-1){
+         for(int a = 0;a<sizeof(hexvals);a++)printf("%c",xored[a] );
+         printf("\n");
+         highest = score;
+      }
+   }
 }
