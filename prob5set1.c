c8b6213bc335de0fd613066bf45c117b8a495ec6
Leah Greenstein <lrg79@cornell.edu>
2016-08-03 10:57:55 -0400

    Update prob5set1.c

--- prob5set1.c
+++ prob5set1.c
@@ -7,15 +7,15 @@ int main(){
 	char xor[] = "ICE";
 
 	for(int i = 0; i < sizeof(toEncrypt)-1; i++){
-		char c = toEncrypt[i];
-		printf("%c", c);
 		int index = i%(sizeof(xor)-1);
 		char c1 = xor[index];
-		encrypted[i] = c1 ^ c;
+		printf("%c", c1);
+		encrypted[i] = c1 ^ toEncrypt[i];
 	}
 
 	for(int i = 0; i < sizeof(encrypted)-1; i++){
-		printf("%c", encrypted[i]);
-	}				
+		printf("%x", encrypted[i]);
+	}
+
 	return 0;
 }
