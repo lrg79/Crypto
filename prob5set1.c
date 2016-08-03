75cdac6e27c3bfb3d0c41a3e5cd4c08569d8ed73
Leah Greenstein <lrg79@cornell.edu>
2016-08-03 10:50:53 -0400

    Update prob5set1.c

--- prob5set1.c
+++ prob5set1.c
@@ -4,20 +4,14 @@ int main(){
 
 	char toEncrypt[] = "Burning 'em, if you ain't quick and nimble I go crazy when I hear a cymbal";
 	char encrypted[sizeof(toEncrypt)-1];
+	char xor[] = "ICE";
 
 	for(int i = 0; i < sizeof(toEncrypt)-1; i++){
 		char c = toEncrypt[i];
-		char c1;
-		if(i%3 == 0){
-			c1 = c ^ 'I';
-		}
-		else if(i%3 == 1){
-			c1 = c ^ 'C';
-		}
-		else if(i%3 == 2){
-			c1 = c ^ 'E';
-		}
-		encrypted[i] = c1;
+		printf("%c", c);
+		int index = i%(sizeof(xor)-1);
+		char c1 = xor[index];
+		encrypted[i] = c1 ^ c;
 	}
 
 	for(int i = 0; i < sizeof(encrypted)-1; i++){
