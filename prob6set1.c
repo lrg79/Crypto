9adb203b655492820761f9bb77b19098e1d514c2
Leah Greenstein <lrg79@cornell.edu>
2016-08-09 11:00:20 -0400

    Update prob6set1.c

--- prob6set1.c
+++ prob6set1.c
@@ -1,20 +1,9 @@
 #include <stdio.h>
-int hamming(char *array, char *array1, int length){
-	int hamming = 0;
 
-	for(int i = 0; i < length; i++){
-		char c = array[i];
-		char c1 = array1[i];
-		for(int j = 0; j < 8; j++){
-			char tmp = 1 << j;
-			int bit1 = (c1 & tmp)>>j;
-			int bit = (c & tmp)>>j;
-			if(bit1 != bit){
-				hamming++;
-			}
-			bit = bit>>1;
-			bit1 = bit1>>1;
-		}
+int hamming(unsigned char *array, unsigned char *array1, int length){
+	int hamming = 0;
+	for(int i = 0; i < length * 8; i++){
+		hamming+=(((array[i/8] >> i%8))^((array1[i/8] >> i%8)))&1;
 	}
 
 	return hamming;
@@ -24,6 +13,6 @@ int main(){
 	char array[] = "this is a test";
 	char array1[] = "wokka wokka!!!";
 	int distance = hamming(array, array1, sizeof(array)-1);
-	printf("%d", distance);
+	printf("\n %d", distance);
 	return 0;
 }
